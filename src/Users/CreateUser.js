c1a351b443e3e2dcfb64e6952d1a5d4a7eec63e0
Sean Malan <104399311+seanmalan@users.noreply.github.com>
2024-11-22 23:26:06 +1300

    customer pages is done

--- src/Users/CreateUser.js
+++ src/Users/CreateUser.js
@@ -7,10 +7,9 @@ const UserCreate = () => {
 
   const [user, setUser] = useState({
     username: "",
-    password: "",
+    password_hash: "",
     email: "",
     emailConfirmed: "",
-    passwordHash: "",
     role: 1,
   });
 
@@ -33,130 +32,128 @@ const UserCreate = () => {
       },
       body: JSON.stringify(user),
     })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Failed to create user.");
-          }
-          return response.json();
-        })
-        .then(() => {
-          alert("User created successfully!");
-          navigate("/users"); // Redirect to users list or another page
-        })
-        .catch((err) => {
-          setError(err.message);
-        });
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to create user.");
+        }
+        return response.json();
+      })
+      .then(() => {
+        alert("User created successfully!");
+        navigate("/users"); // Redirect to users list or another page
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
   };
 
   return (
-
-
+    <div
+      className="d-flex justify-content-center align-items-center vh-100"
+      style={{ backgroundColor: "#d0e7f9" }}
+    >
       <div
-          className="d-flex justify-content-center align-items-center vh-100"
-          style={{backgroundColor: "#d0e7f9"}}
+        className="p-5 rounded shadow"
+        style={{
+          backgroundColor: "#f2f2f2",
+          maxWidth: "600px",
+          width: "100%",
+        }}
       >
-        <div
-            className="p-5 rounded shadow"
-            style={{
-              backgroundColor: "#f2f2f2",
-              maxWidth: "600px",
-              width: "100%",
-            }}
-        >
-          <h1 className="text-center mb-4">Create New User</h1>
-          {error && <div className="alert alert-danger">{`Error: ${error}`}</div>}
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="username" className="form-label">
-                Username:
-              </label>
-              <input
-                  type="text"
-                  className="form-control"
-                  id="username"
-                  name="username"
-                  value={user.username}
-                  onChange={handleChange}
-                  required
-              />
-            </div>
+        <h1 className="text-center mb-4">Create New User</h1>
+        {error && <div className="alert alert-danger">{`Error: ${error}`}</div>}
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              name="username"
+              value={user.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">
-                Password:
-              </label>
-              <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  name="password"
-                  value={user.password}
-                  onChange={handleChange}
-                  required
-              />
-            </div>
+          <div className="mb-3">
+            <label htmlFor="password_hash" className="form-label">
+              Password:
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password_hash"
+              name="password_hash"
+              value={user.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Email:
-              </label>
-              <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  name="email"
-                  value={user.email}
-                  onChange={handleChange}
-                  required
-              />
-            </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email:
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-            <div className="mb-3">
-              <label htmlFor="emailConfirmed" className="form-label">
-                Email Confirmed:
-              </label>
-              <input
-                  type="text"
-                  className="form-control"
-                  id="emailConfirmed"
-                  name="emailConfirmed"
-                  value={user.emailConfirmed}
-                  onChange={handleChange}
-                  required
-              />
-            </div>
-            { user.role === 1 ? (
+          <div className="mb-3">
+            <label htmlFor="emailConfirmed" className="form-label">
+              Email Confirmed:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="emailConfirmed"
+              name="emailConfirmed"
+              value={user.emailConfirmed}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          {user.role === 1 ? (
             <div className="mb-3">
               <label htmlFor="role" className="form-label">
                 Role:
               </label>
               <input
-                  type="number"
-                  className="form-control"
-                  id="role"
-                  name="role"
-                  value="3"
-                  onChange={handleChange}
-                  required
+                type="number"
+                className="form-control"
+                id="role"
+                name="role"
+                value="3"
+                onChange={handleChange}
+                required
               />
             </div>
-                ) : null}
+          ) : null}
 
-            <div className="d-flex justify-content-between align-items-center">
-              <button
-                  type="button"
-                  className="btn btn-secondary"
-                  onClick={() => navigate("/users")}
-              >
-                Back to Users
-              </button>
-              <button type="submit" className="btn btn-primary">
-                Create User
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className="d-flex justify-content-between align-items-center">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => navigate("/users")}
+            >
+              Back to Users
+            </button>
+            <button type="submit" className="btn btn-primary">
+              Create User
+            </button>
+          </div>
+        </form>
       </div>
+    </div>
   );
 };
 
